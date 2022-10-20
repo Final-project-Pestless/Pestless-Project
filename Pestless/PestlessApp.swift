cb2cabbc13ed0a116aa9f716b5153521697f91ee
Nadia Ramadhani <103446529+nadiaramadhani@users.noreply.github.com>
2022-10-20 12:24:12 +0700

    Initial Commit

---
+++ Pestless/PestlessApp.swift
@@ -0,0 +1,20 @@
+//
+//  PestlessApp.swift
+//  Pestless
+//
+//  Created by Local Administrator on 20/10/22.
+//
+
+import SwiftUI
+
+@main
+struct PestlessApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
