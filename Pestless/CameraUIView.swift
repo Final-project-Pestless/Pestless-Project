0cf35f610d4cbb3d6fba40c9029a49522ee8835e
rahmatrizkym <rahmat.118140114@student.itera.ac.id>
2022-10-28 05:39:51 +0700

    + open camera functionality using UIViewController

---
+++ Pestless/CameraUIView.swift
@@ -0,0 +1,57 @@
+//
+//  CameraUIView.swift
+//  Pestless
+//
+//  Created by Rahmat Rizky on 28/10/22.
+//
+
+import SwiftUI
+
+struct CameraUIView: View {
+    
+    @State private var sourceType: UIImagePickerController.SourceType = .photoLibrary
+    @State private var selectedImage: UIImage?
+    @State private var isImagePickerDisplay = false
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                
+                if selectedImage != nil {
+                    Image(uiImage: selectedImage!)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .clipShape(Circle())
+                        .frame(width: 300, height: 300)
+                } else {
+                    Image(systemName: "snow")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .clipShape(Circle())
+                        .frame(width: 300, height: 300)
+                }
+                
+                Button("Camera") {
+                    self.sourceType = .camera
+                    self.isImagePickerDisplay.toggle()
+                }.padding()
+                
+                Button("photo") {
+                    self.sourceType = .photoLibrary
+                    self.isImagePickerDisplay.toggle()
+                }.padding()
+            }
+            .navigationBarTitle("tes add photo")
+            .sheet(isPresented: self.$isImagePickerDisplay) {
+                ImagePickerView(selectedImage: self.$selectedImage, sourceType: self.sourceType)
+            }
+            
+        }
+    }
+}
+
+struct CameraUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        CameraUIView()
+    }
+}
---
+++ Pestless/Coordinator.swift
@@ -0,0 +1,23 @@
+//
+//  Coordinator.swift
+//  Pestless
+//
+//  Created by Rahmat Rizky on 28/10/22.
+//
+
+import UIKit
+
+class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    var picker: ImagePickerView
+    
+    init(picker: ImagePickerView) {
+        self.picker = picker
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let selectedImage = info[.originalImage] as? UIImage else { return }
+        self.picker.selectedImage = selectedImage
+        self.picker.isPresented.wrappedValue.dismiss()
+    }
+    
+}
