5791e02884df4c64fd14e13442bb4d1233c14e7a
Nadia Ramadhani <103446529+nadiaramadhani@users.noreply.github.com>
2022-12-05 13:49:21 +0700

    added new model

--- View/Camera/CameraResultView.swift
+++ View/Camera/CameraResultView.swift
@@ -20,7 +20,7 @@ struct CameraResultView: View {
     @State var percentagePrediction = String()
     @State var percentageDouble: Double = 0
     let cameraService = CameraService()
-    let model = PestClassifier_3()
+    let model = PestDetection()
 //    @Binding var selectedImage: Data?
     @State var isDetected: Bool = false
     @State var isResult: Bool = false
